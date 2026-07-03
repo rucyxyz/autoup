4dfe36b9b46f90dd0a0b6424b01eb5c88537ab33
rucyxyz <rucyxyz@gmail.com>
2026-07-03 15:53:48 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const discord = require("discord.js-selfbot-v13");
+
+const client = new discord.Client({
+  checkUpdate: false,
+});
+
+const upChannelId = "channel id";
+const token = "account token";
+
+client.on("ready", async () => {
+  const upChannel = await client.channels.fetch(upChannelId);
+  await disboardUp();
+  await dissokuUp();
+  await dicoallUp();
+  setTimeout(async function () {
+    await disboardUp();
+    await dissokuUp();
+    await dicoallUp();
+  }, 3610000);
+});
+
+async function disboardBump(upChannel) {
+  await upChannel.sendSlash('302050872383242240', 'bump');
+  console.log('disboard bump!');
+}
+
+async function dissokuUp(upChannel) {
+  await upChannel.sendSlash('761562078095867916', 'dissoku up');
+  console.log('dissoku up!');
+}
+
+async function dicoallUp(upChannel) {
+  await upChannel.sendSlash('903541413298450462', 'up')/
+  console.log('dicoall up!');
+}
+
+/* -------------------- Login --------------------*/
+
+client.login(token);
